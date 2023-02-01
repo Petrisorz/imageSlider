66e038f5972bfcedc207e60b2a8b0ee92eabbcef
Petrisorz <petrisorfrumuzache@gmail.com>
2023-02-01 18:46:21 +0200

    second

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
-const { resolve } = require('path');
 const path = require ('path');
 module.exports = {
     entry:'./src/index.js',
     output:{
         filename: 'bundle.js',
-        path : resolve(__dirname,'dist'),
+        path : path.resolve(__dirname,'dist'),
     },
 };
